50a1d8976e16fca7d0b7cf0b73df0d9d945b47af
Dhyan Patel <dhyanbpatel2005@gmail.com>
2025-07-18 09:30:14 +0530

    modified:   backend/package.json 	new file:   backend/src/db/index.js 	modified:   backend/src/index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,13 @@
-import mongoose from 'mongoose'
-import { DB_NAME } from './constants'
 
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
 
 /*
 import express from 'express'
@@ -23,3 +30,4 @@ const app = express()
     }
 })()
     */
+
